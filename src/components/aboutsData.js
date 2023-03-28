369906f93ce09b63b10935f5891ed30290a20ae2
lightlai <taralai@outlook.com>
2023-03-28 19:08:06 -0400

    about updated

--- src/components/aboutsData.js
+++ src/components/aboutsData.js
@@ -3,6 +3,6 @@ export const aboutData = [
     "id": 1,
     "title": "Full-stack developer",
     "image": require("./img/portrait.png"),
-    "content": "After a couple of years of working experience in finance and hospitality, I felt something was missing. Throughout the years, I noticed myself loving to solve problems around me academically. I was also inspired by some friends of mine that are professionals in programming and blockchain. I decided to start this path this year as well. Among other programming fields, I thought web development was the best choice since I am also interested in designing. My love for problem-solving and my interest in art and design make me a well-rounded and devoted Web Developer.",
+    "content": "Hi, I'm Tara Lai, a web developer with experience in designing, developing, and maintaining user-friendly websites and applications. My skills in full-stack development, UX/UI design, and eCommerce integration have enabled me to deliver exceptional results. This portfolio showcases some of my notable projects, including a dynamic event-discovery web application and a responsive weather app. Thank you for visiting, and I hope you enjoy browsing through my work!",
   }
 ]
