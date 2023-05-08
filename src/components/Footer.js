2752d10f59897ed20c884a5bff40f512a238ca90
lightlai <taralai@outlook.com>
2023-05-08 01:29:41 -0400

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { FaCopyright} from 'react-icons/fa';
 function Footer() {
   return(
     <FooterStyle className="footer">
-      <div className="footer-copy"><FaCopyright/> 2022 Tara Lai.</div>
+      <div className="footer-copy"><FaCopyright/> 2023 Tara Lai.</div>
       <ProfileLinks/>
     </FooterStyle>  
   )
